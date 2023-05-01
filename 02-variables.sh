121b176269e48f49d2c289396ca9a729964dc744
kingNomad3 <e.bjoinvil@etu.cvm.qc.ca>
2023-05-01 11:23:21 -0400

    exercice 1

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-#pour affecter une variable il ne faut 
+#pour affecter une variable il ne faut pas mettre d'espace
+
+prenom=Benjamin
+
+echo $prenom
+# declare rarement utiliser
+declare -i age=33
+# -r donne une constante
+declare -r CHEMIN=/bin
+
+# $0 donn 
