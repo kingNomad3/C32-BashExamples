9d31dc4bbc6f8cce505e3fe01340c7e5a7a57bfc
kingNomad3 <e.bjoinvil@etu.cvm.qc.ca>
2023-05-01 11:26:10 -0400

    exercice1

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+
+# afficher un menu
+# mettre le menu dans une fonction
+#offire 2 choix :quitter (choixq)
+# ou appelez ./execice2.sh (choix d)
+# a chaqe debut de boucle afficber le menu
+
+read -p "Entrez votre choix: " choix
+
+
+while[[ $resultat = o ]]
+do 
+read -p "choix : " resultat
